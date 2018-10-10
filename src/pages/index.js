2ee7e4290f868a5c4f6602de2545ae1c58e9b2e4
sizhao <870301137@qq.com>
2018-10-11 01:49:29 +0800

    chore: finish the part three of the gatsby tutorial

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,12 +1,30 @@
-import React from "react"
-import Header from "../components/header"
-import { Link } from "gatsby"
+import React from 'react'
+import { Link } from 'gatsby'
 
 export default () => (
-  <div style={{ color: "purple" }}>
-    <Link to="/contact/">Contact</Link>
-    <Header content="Hello, Gatsby!" />
-    <p>What a world!</p>
-    <img src="https://source.unsplash.com/random/400x200" alt="" />
+  <div style={{ margin: `3rem auto`, maxWidth: 600 }}>
+    <Link to='/about-css-modules'>CSS Modules</Link>
+    <h1>Richard Hamming on Luck</h1>
+    <div>
+      <p>
+        From Richard Hamming’s classic and must-read talk, “
+        <a href="http://www.cs.virginia.edu/~robins/YouAndYourResearch.html">
+          You and Your Research
+        </a>
+        ”.
+      </p>
+      <blockquote>
+        <p>
+          There is indeed an element of luck, and no, there isn’t. The prepared
+          mind sooner or later finds something important and does it. So yes, it
+          is luck.{" "}
+          <em>
+            The particular thing you do is luck, but that you do something is
+            not.
+          </em>
+        </p>
+      </blockquote>
+    </div>
+    <p>Posted April 09, 2011</p>
   </div>
 )
---
+++ src/utils/typography.js
@@ -0,0 +1,7 @@
+import Typography from 'typography'
+// import bootstrapTheme from 'typography-theme-bootstrap'
+import awtonTheme from 'typography-theme-lawton'
+
+const typography = new Typography(awtonTheme)
+
+export default typography
