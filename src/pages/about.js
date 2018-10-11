f67642cd955a3367f5c3afd12568402bdde12c12
sizhao <870301137@qq.com>
2018-10-11 21:19:03 +0800

    feat: explore the css-in-js library - emotion

--- src/pages/about.js
+++ src/pages/about.js
@@ -1,11 +1,16 @@
 import React, { Component } from "react"
 import Header from "../components/header"
-import Layout from '../components/Layout'
+import Layout from '../components/LayoutCSSIn'
 
 export default class About extends Component {
   render() {
     return (
       <Layout>
+        <h1>About Pandas Eating Lots</h1>
+        <p>
+          We're the only site running on your computer dedicated to showing the best
+          photos and videos of pandas eating lots of food.
+        </p>
         <Header content="About me!" />
         <Header content="It's pretty code~" />
         <p>Such wow, Very React.</p>
--- src/utils/typography.js
+++ src/utils/typography.js
@@ -1,8 +1,11 @@
 import Typography from 'typography'
 // import bootstrapTheme from 'typography-theme-bootstrap'
 // import awtonTheme from 'typography-theme-lawton'
-import fairyGatesTheme from 'typography-theme-fairy-gates'
+// import fairyGatesTheme from 'typography-theme-fairy-gates'
+import kirkhamTheme from 'typography-theme-kirkham'
 
-const typography = new Typography(fairyGatesTheme)
+const typography = new Typography(kirkhamTheme)
 
 export default typography
+
+export const rhythm = typography.rhythm
